300394e3b04ab68d8dce9d29470c5e5f52569148
patrick <patrick@makanda.io>
2020-12-10 00:13:40 +0100

    add android project

---
+++ XamFormsFirebaseAuth.Android/FirebaseAuthentication.cs
@@ -0,0 +1,48 @@
+﻿using Firebase.Auth;
+using System;
+using System.Threading.Tasks;
+using XamFormsFirebaseAuth.Features.Common;
+
+namespace XamFormsFirebaseAuth.Android
+{
+    public class FirebaseAuthentication : IAuthenticationService
+    {
+        public async Task<bool> CreateUser(string username, string email, string password)
+        {
+            try
+            {
+                var authResult = await FirebaseAuth.Instance
+                    .CreateUserWithEmailAndPasswordAsync(email, password);
+
+                var userProfileChangeRequestBuilder = new UserProfileChangeRequest.Builder();
+                userProfileChangeRequestBuilder.SetDisplayName(username);
+
+                var userProfileChangeRequest = userProfileChangeRequestBuilder.Build();
+                await authResult.User.UpdateProfileAsync(userProfileChangeRequest);
+
+                return await Task.FromResult(true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return await Task.FromResult(false);
+            }
+        }
+
+        public bool IsSignIn() 
+            => FirebaseAuth.Instance.CurrentUser != null;
+
+        public async Task ResetPassword(string email) 
+            => await FirebaseAuth.Instance.SendPasswordResetEmailAsync(email);
+
+        public async Task<string> SignIn(string email, string password)
+        {
+            var authResult = await FirebaseAuth.Instance.SignInWithEmailAndPasswordAsync(email, password);
+            var token =  await authResult.User.GetIdTokenAsync(false);
+            return token.Token;
+        }
+
+        public void SignOut() 
+            => FirebaseAuth.Instance.SignOut();
+    }
+}
---
+++ XamFormsFirebaseAuth.Android/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿
+using Android.App;
+using Android.Runtime;
+using Android.OS;
+using Firebase;
+using Android.Content.PM;
+using System;
+
+namespace XamFormsFirebaseAuth.Android
+{
+    [Activity(Label = "XAMFORMS", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            try
+            {
+                TabLayoutResource = Resource.Layout.Tabbar;
+                ToolbarResource = Resource.Layout.Toolbar;
+
+                base.OnCreate(savedInstanceState);
+
+                Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+                global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+                FirebaseApp.InitializeApp(Application.Context);
+                LoadApplication(new App());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+           
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
